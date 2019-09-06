712c82346f0922217bd411b08f4b0d382a763e04
selvadharani-it19 <selvadharani.it19@bitsathy.ac.in>
2019-09-06 12:16:04 +0530

    1

---
+++ cutoff.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+printf("%f",92+70/2+86/2);
+}
---
+++ one.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main()
+{
+printf("\t\t\t1\n\t\t1\t2\t3\n\t1\t2\t3\t2\t1\n1\t2\t3\t4\t3\t2\t1");
+}
